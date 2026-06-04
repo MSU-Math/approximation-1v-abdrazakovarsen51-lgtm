dd0f66a53998dae0d2e03c64bb622b9d909b2be5
abdrazakovarsen51-lgtm <abdrazakovarsen51@gmail.com>
2026-06-04 16:54:39 +0300

    new

--- hermite.cpp
+++ hermite.cpp
@@ -1,4 +1,5 @@
 #include "hermite.h"
+#include <stddef.h>
 
 static int find_segment(double x, int n, const double *x_nodes)
 {
@@ -26,8 +27,7 @@ static int find_segment(double x, int n, const double *x_nodes)
 int hermite_init(int n, const double *x, const double *f,
                  const double *df, double *coef)
 {
-    if (n < 4 || x == nullptr || f == nullptr || df == nullptr ||
-        coef == nullptr)
+    if (n < 4 || x == NULL || f == NULL || df == NULL || coef == NULL)
         return -1;
 
     for (int i = 1; i < n; i++) {
@@ -75,14 +75,11 @@ int hermite_init(int n, const double *x, const double *f,
 double hermite_compute(double x, double a, double b, int n,
                        const double *x_nodes, const double *coef)
 {
-    if (n < 2 || x_nodes == nullptr || coef == nullptr)
+    (void)a;
+    (void)b;
+
+    if (n < 2 || x_nodes == NULL || coef == NULL)
         return 0.0;
-
-    if (x < a)
-        x = a;
-
-    if (x > b)
-        x = b;
 
     int i = find_segment(x, n, x_nodes);
     double t = x - x_nodes[i];
